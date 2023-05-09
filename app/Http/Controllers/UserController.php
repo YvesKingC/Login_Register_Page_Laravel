3ee867ab7885bb1a9600609d9b785ebc75a23b7d
Kyrell <kleanosiauw@student.ciputra.ac.id>
2023-05-09 20:28:53 +0700

    #1 (userController, model) ngubah model, users, DB

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,30 +2,38 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+
 class UserController extends Controller
 {
     //
     public function register(Request $request)
     {
+        // dd($request->all());
         // Validate the form data
-        $validatedData = $request->validate([
-            'fullname' => 'required|max:255',
-            'username' => 'required|unique:webusers|max:255',
-            'email' => 'required|unique:webusers|email|max:255',
+        $request->validate([
+            'name' => 'required|max:255',
+            'username' => 'required|unique:users|max:255',
+            'email' => 'required|unique:users|email|max:255',
             'password' => 'required|max:255',
         ]);
 
         // Create a new user
-        $user = new User;
-        $user->fullname = $request->fullname;
-        $user->username = $request->username;
-        $user->email = $request->email;
-        $user->password = bcrypt($request->password);
-        $user->save();
+        $user = User::create([
+            'name' => $request->name,
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
 
-        // Redirect the user after successful registration
-        return redirect()->route('home');
+        if ($user) {
+            // Log the user in
+            auth()->login($user);
+            // Redirect to the dashboard
+            return redirect()->route('home');
+        }
     }
 }
